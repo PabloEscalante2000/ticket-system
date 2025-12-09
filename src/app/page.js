9f60d43c3e286362cc5419d5cbda042fbc8f9c21
Pablo Escalante <pabloescalante200027@gmail.com>
2025-12-09 16:54:47 -0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
+"use client"
+
 import Image from "next/image";
+import { createSupabaseBrowserClient } from "@/supabase-utils/browserClient";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    const supabase = createSupabaseBrowserClient();
+    supabase.storage.listBuckets().then((result) =>
+     console.log("Bucket List", result)
+    )
+  }, []);
+
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
