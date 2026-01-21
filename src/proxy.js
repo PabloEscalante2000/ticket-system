6e47e7f4c07d7a93515f6746ccbd63c9826a9770
Pablo Escalante <pabloescalante200027@gmail.com>
2026-01-21 17:23:25 -0500

    cambios req

--- src/proxy.js
+++ src/proxy.js
@@ -4,12 +4,22 @@ export async function proxy(req) {
   const { supabase, response } = getSupabaseReqResClient({ request:req });
   const session = await supabase.auth.getSession();
   const requestedPath = req.nextUrl.pathname;
+
+  // Separar el tenantID del requestedPath
+  const [tenant, ...restOfPath] = requestedPath.substr(1).split("/")
+  const applicationPath = "/" + restOfPath.join("/");
+
+  //Asegurandose que este bien escrito el tenant
+  if ( !/[a-z0-9-_]+/.test(tenant) ) {
+    return NextResponse.rewrite(new URL("/not-found", req.url));
+  }
+
   const sessionUser = session.data?.session?.user;
-  if (requestedPath.startsWith("/tickets")) {
+  if (applicationPath.startsWith("/tickets")) {
     if (!sessionUser) {
       return NextResponse.redirect(new URL("/", req.url));
     }
-  } else if (requestedPath === "/") {
+  } else if (applicationPath === "/") {
     if (sessionUser) {
       return NextResponse.redirect(new URL("/tickets", req.url));
     }
